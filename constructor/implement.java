e360ec92699169661c086d98891d5463ea1d7dfb
Yslain3 <yslaine.carvalho28@gmail.com>
2025-12-15 22:04:57 -0300

    Implement MontadorSUVFamiliar class for vehicle assembly

--- constructor/implement.java
+++ constructor/implement.java
@@ -8,6 +8,50 @@ package com.mycompany.constructor;
  *
  * @author yslai
  */
-public class implement {
-    
+
+package construtor_atv3;
+
+class MontadorSUVFamiliar implements Montador {
+
+    private Veiculo veiculo;
+
+    public MontadorSUVFamiliar() {
+        preparar();
+    }
+
+    @Override
+    public void iniciar() {
+        preparar();
+    }
+
+    private void preparar() {
+        veiculo = new Veiculo();
+        veiculo.setCategoria("Familiar SUV");
+    }
+
+    @Override
+    public void definirMotor(String tipoMotor) {
+        veiculo.setMotor(tipoMotor);
+    }
+
+    @Override
+    public void definirGPS(String sistemaGPS) {
+        veiculo.setGPS(sistemaGPS);
+    }
+
+    @Override
+    public void definirPainel(String tipoPainel) {
+        veiculo.setPainelDigital(tipoPainel);
+    }
+
+    @Override
+    public void definirAssentos(int quantidade) {
+        veiculo.setAssentos(quantidade);
+    }
+
+    @Override
+    public Veiculo resultado() {
+        return veiculo;
+    }
 }
+
