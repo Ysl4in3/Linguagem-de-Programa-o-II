547c7edad502e247b11171b49f60c9c998d9d99e
Yslain3 <yslaine.carvalho28@gmail.com>
2025-12-15 22:07:30 -0300

    Add MontadorExecutivoPremium class implementation

--- constructor/montador.java
+++ constructor/montador.java
@@ -8,6 +8,49 @@ package com.mycompany.constructor;
  *
  * @author yslai
  */
-public class montador {
-    
+package construtor_atv3;
+
+class MontadorExecutivoPremium implements Montador {
+
+    private Veiculo veiculo;
+
+    public MontadorExecutivoPremium() {
+        preparar();
+    }
+
+    @Override
+    public void iniciar() {
+        preparar();
+    }
+
+    private void preparar() {
+        veiculo = new Veiculo();
+        veiculo.setCategoria("Executivo Longo");
+    }
+
+    @Override
+    public void definirMotor(String tipoMotor) {
+        veiculo.setMotor(tipoMotor);
+    }
+
+    @Override
+    public void definirGPS(String sistemaGPS) {
+        veiculo.setGPS(sistemaGPS);
+    }
+
+    @Override
+    public void definirPainel(String tipoPainel) {
+        veiculo.setPainelDigital(tipoPainel);
+    }
+
+    @Override
+    public void definirAssentos(int quantidade) {
+        veiculo.setAssentos(quantidade);
+    }
+
+    @Override
+    public Veiculo resultado() {
+        return veiculo;
+    }
 }
+
