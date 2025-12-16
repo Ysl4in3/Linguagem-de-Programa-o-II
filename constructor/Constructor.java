ceccba707154a51be6c0c60a68c363a243f938c2
Yslain3 <yslaine.carvalho28@gmail.com>
2025-12-15 22:03:47 -0300

    Add DiretorDeConstrucao class with construction methods

--- constructor/Constructor.java
+++ constructor/Constructor.java
@@ -8,9 +8,33 @@ package com.mycompany.constructor;
  *
  * @author yslai
  */
-public class Constructor {
+package construtor_atv3;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+class DiretorDeConstrucao {
+
+    public void construirModeloUrbano(Montador montador) {
+        montador.iniciar();
+        montador.definirAssentos(4);
+        montador.definirMotor("1.0 Flex");
+    }
+
+    public void construirModeloEsportivo(Montador montador) {
+        montador.iniciar();
+        montador.definirAssentos(2);
+        montador.definirMotor("2.4 Turbo");
+    }
+
+    public void construirModeloSUV(Montador montador) {
+        montador.iniciar();
+        montador.definirAssentos(7);
+        montador.definirMotor("3.0 V6");
+    }
+
+    public void construirModeloExecutivo(Montador montador) {
+        montador.iniciar();
+        montador.definirAssentos(4);
+        montador.definirMotor("2.0 Premium");
+        montador.definirGPS("GPS Integrado");
+        montador.definirPainel("Painel Executivo");
     }
 }
