d1ebf9cc23e599deb60c61f669e4ce4ec5368864
Yslain3 <yslaine.carvalho28@gmail.com>
2025-12-15 22:08:28 -0300

    Update Construtor.java

--- construtor/Construtor.java
+++ construtor/Construtor.java
@@ -8,9 +8,40 @@ package com.mycompany.construtor;
  *
  * @author yslai
  */
-public class Construtor {
+package construtor;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import java.util.ArrayList;
+import java.util.List;
+
+public class CadastroFesta {
+
+    private static CadastroFesta instancia;
+
+    private List<Pessoa> listaConvidados;
+
+    private CadastroFesta() {
+        listaConvidados = new ArrayList<>();
+        carregarConvidados();
+    }
+
+    public static CadastroFesta obterInstancia() {
+        if (instancia == null) {
+            instancia = new CadastroFesta();
+        }
+        return instancia;
+    }
+
+    private void carregarConvidados() {
+        listaConvidados.add(new Pessoa("Luiz"));
+        listaConvidados.add(new Pessoa("José"));
+        listaConvidados.add(new Pessoa("Maria"));
+        listaConvidados.add(new Pessoa("João"));
+    }
+
+    public void mostrarConvidados() {
+        System.out.println("== CONVIDADOS DA FESTA ==");
+        for (Pessoa pessoa : listaConvidados) {
+            System.out.println("- " + pessoa.getNome());
+        }
     }
 }
