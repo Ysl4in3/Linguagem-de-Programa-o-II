08ebbac6422db8ad336110b842d0e50cb8b42794
Yslain3 <yslaine.carvalho28@gmail.com>
2025-12-15 22:10:23 -0300

    Refactor Observer class to include Observador interface
    Removed main method and added Observador interface.

--- observer/Observer.java
+++ observer/Observer.java
@@ -8,9 +8,9 @@ package com.mycompany.observer;
  *
  * @author yslai
  */
-public class Observer {
+package observer;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+public interface Observador {
+
+    void atualizar(Produto produto);
 }
