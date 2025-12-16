a4e7ad1d86d055da32fc2571776e07ea07fa2843
Yslain3 <yslaine.carvalho28@gmail.com>
2025-12-15 22:02:25 -0300

    Implement adapter for Cubo to Cilindro conversion

--- adapter/cubocilindro.java
+++ adapter/cubocilindro.java
@@ -8,6 +8,21 @@ package com.mycompany.adapter;
  *
  * @author yslai
  */
-public class cubocilindro {
-    
+package adapter;
+
+public class AdaptadorCuboCilindrico extends Cilindro {
+
+    private Cubo cuboAdaptado;
+
+    public AdaptadorCuboCilindrico(Cubo cubo) {
+        this.cuboAdaptado = cubo;
+    }
+
+    @Override
+    public int obterRaio() {
+        // raio equivalente calculado a partir da diagonal do cubo
+        double diagonalCubo = cuboAdaptado.obterLado() * Math.sqrt(2);
+        return (int) (diagonalCubo / 2);
+    }
 }
+
