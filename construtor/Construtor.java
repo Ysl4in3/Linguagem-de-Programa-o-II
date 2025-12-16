c25651a51fbd7031b6203369e2642d2f57e91181
Yslain3 <yslaine.carvalho28@gmail.com>
2025-12-15 21:46:22 -0300

    Add files via upload

---
+++ construtor/Construtor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.construtor;
+
+/**
+ *
+ * @author yslai
+ */
+public class Construtor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
